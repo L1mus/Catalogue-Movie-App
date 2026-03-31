7ce20a9f273fb7a5e29e78126fad07ebea7f7565
l1mus <alinesezcore@gmail.com>
2026-03-31 08:24:00 +0700

    feat: create API endpoint handler function to protect sensitive data

---
+++ api/tmdb.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+  const { endpoint, ...queryParams } = req.query;
+
+  if (!endpoint) {
+    return res.status(400).json({ error: "Endpoint is required" });
+  }
+
+  const queryString = new URLSearchParams(queryParams).toString();
+  const url = `https://api.themoviedb.org/3${endpoint}?${queryString}`;
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
